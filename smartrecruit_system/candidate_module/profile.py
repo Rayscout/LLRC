212ff2eebbbc14c4ccfeab46aa0df7720bf6a7bc
1aknn <2462123510@qq.com>
2025-08-21 14:24:49 +0800

    可上传头像并保存

--- smartrecruit_system/candidate_module/profile.py
+++ smartrecruit_system/candidate_module/profile.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, g, current_app
 from werkzeug.utils import secure_filename
 import os
+import time
 from app.models import User, db
 from app.utils import allowed_file, get_allowed_cv_extensions, extract_text_from_resume
 
@@ -15,6 +16,12 @@ def settings():
     
     if request.method == 'POST':
         action = request.form.get('action')
+        # 兼容前端未传递 action 的情况，根据按钮名称回退
+        if not action:
+            if 'save_changes' in request.form:
+                action = 'update_info'
+            elif 'upload_cv' in request.form:
+                action = 'upload_cv'
         
         if action == 'update_info':
             # 更新基本信息
@@ -24,20 +31,36 @@ def settings():
             g.user.phone_number = request.form['phone_number']
             g.user.birthday = request.form['birthday']
             g.user.position = request.form.get('position', '')
+            # 同时处理头像上传（如果选择了文件）
+            if 'profile_photo' in request.files:
+                file = request.files['profile_photo']
+                if file and file.filename and allowed_file(file.filename, {'png', 'jpg', 'jpeg', 'gif'}):
+                    # 使用用户ID与时间戳生成唯一文件名，避免覆盖
+                    original_ext = os.path.splitext(secure_filename(file.filename))[1].lower()
+                    unique_name = f"user_{g.user.id}_{int(time.time())}{original_ext}"
+                    filepath = os.path.join(current_app.config['UPLOAD_FOLDER_PHOTOS'], unique_name)
+                    file.save(filepath)
+                    g.user.profile_photo = unique_name
+                elif file and not file.filename:
+                    pass  # 未选择文件时忽略
+                elif file and file.filename:
+                    flash('请选择有效的图片文件。', 'danger')
+                    return redirect(url_for('smartrecruit.candidate.profile.settings'))
             
             db.session.commit()
             flash('个人信息更新成功！', 'success')
             return redirect(url_for('smartrecruit.candidate.profile.settings'))
             
         elif action == 'upload_photo':
-            # 上传头像
+            # 上传头像（备用：如果前端单独提交头像表单）
             if 'profile_photo' in request.files:
                 file = request.files['profile_photo']
                 if file and file.filename and allowed_file(file.filename, {'png', 'jpg', 'jpeg', 'gif'}):
-                    filename = secure_filename(file.filename)
-                    filepath = os.path.join(current_app.config['UPLOAD_FOLDER_PHOTOS'], filename)
+                    original_ext = os.path.splitext(secure_filename(file.filename))[1].lower()
+                    unique_name = f"user_{g.user.id}_{int(time.time())}{original_ext}"
+                    filepath = os.path.join(current_app.config['UPLOAD_FOLDER_PHOTOS'], unique_name)
                     file.save(filepath)
-                    g.user.profile_photo = filename
+                    g.user.profile_photo = unique_name
                     db.session.commit()
                     flash('头像上传成功！', 'success')
                 else:
