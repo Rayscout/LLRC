0f7396cdd5d1dd1a192892e063873b1c09c7ae7f
1aknn <2462123510@qq.com>
2025-08-22 15:31:32 +0800

    加入首页功能

--- smartrecruit_system/candidate_module/routes.py
+++ smartrecruit_system/candidate_module/routes.py
@@ -1,10 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, g
 from .profile import profile_bp
 from .jobs import jobs_bp
 from .applications import applications_bp
 
 # 创建求职者主蓝图
 candidate_bp = Blueprint('candidate', __name__, url_prefix='/candidate')
+
+@candidate_bp.route('/')
+def home():
+    """候选人首页 - 智能推荐"""
+    if g.user is None:
+        from flask import redirect, url_for, flash
+        flash('请先登录。', 'danger')
+        return redirect(url_for('common.auth.sign'))
+
+    try:
+        from .jobs import extract_user_skills
+        user_skills = extract_user_skills(g.user)
+    except Exception:
+        user_skills = []
+
+    return render_template('smartrecruit/candidate/home.html', user_skills=user_skills)
 
 # 注册求职者子蓝图
 candidate_bp.register_blueprint(profile_bp)
